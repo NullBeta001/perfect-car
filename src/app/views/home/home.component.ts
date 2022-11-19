bfb742defb624c49a5863b6800c775db746a4f05
NullBeta001 <fabiooli221297@gmail.com>
2022-11-19 19:44:59 -0300

    compilações finais

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -11,16 +11,16 @@ export interface CadastroVeiculo {
 }
 
 const ELEMENT_DATA: CadastroVeiculo[] = [
-  {ano: 2020, modelo: 'Gol', cor: "Preto", marca: 'VW'},
-  {ano: 2010, modelo: 'Vectra', cor: "Prata", marca: 'GM'},
-  {ano: 2006, modelo: 'Astra', cor: "Branco", marca: 'GM'},
-  {ano: 2005, modelo: 'Passat', cor: "Vermelho", marca: 'VW'},
-  {ano: 2021, modelo: 'Civic', cor: "Verde", marca: 'Honda'},
-  {ano: 2000, modelo: 'Hilux', cor: "Azul", marca: 'Toyota'},
-  {ano: 2012, modelo: 'CRV', cor: "Branco", marca: 'Honda'},
-  {ano: 2009, modelo: 'City', cor: "Branco", marca: 'Honda'},
-  {ano: 2022, modelo: 'Tiguan', cor: "Prata", marca: 'VW'},
-  {ano: 2019, modelo: 'Amarok', cor: "Verde", marca: 'VW'},
+  { ano: 1, modelo: 'Gol', cor: "Preto", marca: 'VW' },
+  { ano: 2, modelo: 'Vectra', cor: "Prata", marca: 'GM' },
+  { ano: 3, modelo: 'Astra', cor: "Branco", marca: 'GM' },
+  { ano: 4, modelo: 'Passat', cor: "Vermelho", marca: 'VW' },
+  { ano: 5, modelo: 'Civic', cor: "Verde", marca: 'Honda' },
+  { ano: 6, modelo: 'Hilux', cor: "Azul", marca: 'Toyota' },
+  { ano: 7, modelo: 'CRV', cor: "Branco", marca: 'Honda' },
+  { ano: 8, modelo: 'City', cor: "Branco", marca: 'Honda' },
+  { ano: 9, modelo: 'Tiguan', cor: "Prata", marca: 'VW' },
+  { ano: 10, modelo: 'Amarok', cor: "Verde", marca: 'VW' },
 ];
 
 @Component({
@@ -41,26 +41,36 @@ export class HomeComponent implements OnInit {
 
   openDialog(element: CadastroVeiculo | null): void {
     const dialogRef = this.dialog.open(ElementDialogComponent, {
-      width: '250px',
+      width: '280px',
       data: element === null ? {
         ano: null,
         modelo: '',
         cor: '',
         marca: ''
-      } : element
+      } : {
+        ano: element.ano,
+        modelo: element.modelo,
+        cor: element.cor,
+        marca: element.marca
+      }
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      if(result !== undefined) {
-        this.dataSource.push(result);
-        this.table.renderRows();
-      }  
+      if (result !== undefined) {
+        if (this.dataSource.map(p => p.ano).includes(result.ano)) {
+          this.dataSource[result.ano - 1] = result;
+          this.table.renderRows();
+        } else {
+          this.dataSource.push(result);
+          this.table.renderRows();
+        }
+      }
     });
   }
 
   editElement(element: CadastroVeiculo): void {
     this.openDialog(element);
-  } 
+  }
 
   deleteElement(ano: number): void {
     this.dataSource = this.dataSource.filter(p => p.ano !== ano);
